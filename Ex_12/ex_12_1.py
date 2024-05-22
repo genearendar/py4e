c05265489c62644bb5e00303a18c34a356021af4
Gene Arendarenko <genearendar@gmail.com>
2024-05-22 20:28:20 +1200

    Finished Ex 12_1

---
+++ Ex_12/ex_12_1.py
@@ -0,0 +1,24 @@
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import re
+
+def enter_url():
+    url = input('Enter url:')
+    if re.search('^https?://[^\s/$.?#].[^\s]*$', url):
+        return url
+    else:
+        print('Not a valid url')
+        enter_url()
+url = enter_url()
+try:
+    html = urllib.request.urlopen(url).read()
+except:
+    print('Can\'t open the url')
+    quit()
+soup_html = BeautifulSoup(html, 'html.parser')
+numbers = soup_html('span')
+numbers = [int(number.text) for number in numbers]
+print(sum(numbers))
+
+
+
